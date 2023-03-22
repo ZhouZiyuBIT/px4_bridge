cfe351e862987cdbd8f90b53098b103a1c078586
ZhouZiyuBIT <ZhouZiyu_BIT@outlook.com>
2023-03-22 15:19:52 +0800

    v

---
+++ script/q_sim.py
@@ -0,0 +1,78 @@
+#! /usr/bin/python3.8
+
+import numpy as np
+import casadi as ca
+import time
+
+# ROS
+import rospy
+import tf
+
+from px4_bridge.msg import ThrustRates
+from nav_msgs.msg import Odometry
+
+from quadrotor import QuadrotorModel, QuadrotorSim
+
+# 
+import os, sys
+BASEPATH = os.path.abspath(__file__).split('script', 1)[0]
+# sys.path += [BASEPATH]
+
+#######################################################################################################
+rospy.init_node("q_sim")
+rospy.loginfo("ROS: Hello")
+
+thrust_rates_msg =ThrustRates()
+def thrust_rates_cb(msg:ThrustRates):
+    global thrust_rates_msg
+    thrust_rates_msg = msg
+    pass
+
+thrust_rates_sub = rospy.Subscriber("~thrust_rates", ThrustRates, callback=thrust_rates_cb, queue_size=1, tcp_nodelay=True)
+
+quad = QuadrotorModel(BASEPATH+"config/quad_real.yaml")
+q_sim = QuadrotorSim(quad)
+
+tf_br = tf.TransformBroadcaster(queue_size=1)
+odom_pub = rospy.Publisher("~odom", Odometry, tcp_nodelay=True, queue_size=1)
+
+def sim_run(e):
+    # rospy.loginfo("run")
+    global thrust_rates_msg
+    
+    if thrust_rates_msg != None:
+
+        u = np.zeros(4)
+        u[0] = thrust_rates_msg.thrust*quad._T_max  
+        u[1] = thrust_rates_msg.wx
+        u[2] = thrust_rates_msg.wy
+        u[3] = thrust_rates_msg.wz
+        q_sim.step10ms(u)
+
+        q_state = q_sim._X
+        odom_msg = Odometry()
+        odom_msg.header.frame_id="world"
+        odom_msg.header.stamp = rospy.Time.now()
+        odom_msg.pose.pose.position.x = q_state[0]
+        odom_msg.pose.pose.position.y = q_state[1]
+        odom_msg.pose.pose.position.z = q_state[2]
+        odom_msg.twist.twist.linear.x = q_state[3]
+        odom_msg.twist.twist.linear.y = q_state[4]
+        odom_msg.twist.twist.linear.z = q_state[5]
+        odom_msg.pose.pose.orientation.w = q_state[6]
+        odom_msg.pose.pose.orientation.x = q_state[7]
+        odom_msg.pose.pose.orientation.y = q_state[8]
+        odom_msg.pose.pose.orientation.z = q_state[9]
+        odom_msg.twist.twist.angular.x = q_state[10]
+        odom_msg.twist.twist.angular.y = q_state[11]
+        odom_msg.twist.twist.angular.z = q_state[12]
+        odom_pub.publish(odom_msg)
+
+        tf_br.sendTransform((q_state[1],q_state[0],-q_state[2]), (q_state[8],q_state[7],-q_state[9],q_state[6]), rospy.Time.now(), "quad_body", "world")
+    pass
+    
+
+timer = rospy.Timer(rospy.Duration(0.01), sim_run, oneshot=False, reset=False)
+
+rospy.spin()
+rospy.loginfo("ROS: Goodby")
---
+++ src/px4_bridge_node.cpp
@@ -0,0 +1,109 @@
+#include "Px4Bridge.hpp"
+
+#include<iostream>
+#include<string>
+
+#include<ros/ros.h>
+#include<sensor_msgs/Imu.h>
+#include<nav_msgs/Odometry.h>
+#include"px4_bridge/ThrustRates.h"
+#include<tf/transform_broadcaster.h>
+
+Px4Bridge quad;
+
+ros::Publisher _imu_pub;
+ros::Publisher _state_pub;
+ros::Subscriber _slam_odom_sub;
+ros::Subscriber _thrust_rates_sub;
+tf::TransformBroadcaster* tf_br;
+
+void rcv_imu_callback(float w[3], float a[3])
+{
+    sensor_msgs::Imu s_imu;
+    s_imu.header.stamp = ros::Time::now();
+    s_imu.angular_velocity.x = w[0];
+    s_imu.angular_velocity.y = w[1];
+    s_imu.angular_velocity.z = w[2];
+    s_imu.linear_acceleration.x = a[0];
+    s_imu.linear_acceleration.y = a[1];
+    s_imu.linear_acceleration.z = a[2];
+
+    quad._angular_rate[0] = w[0];
+    quad._angular_rate[1] = w[1];
+    quad._angular_rate[2] = w[2];
+    _imu_pub.publish(s_imu);
+}
+
+void rcv_state_callback(float state[13])
+{
+    nav_msgs::Odometry odom;
+    odom.pose.pose.position.x = state[0];
+    odom.pose.pose.position.y = state[1];
+    odom.pose.pose.position.z = state[2];
+    odom.twist.twist.linear.x = state[3];
+    odom.twist.twist.linear.y = state[4];
+    odom.twist.twist.linear.z = state[5];
+    odom.pose.pose.orientation.w = state[6];
+    odom.pose.pose.orientation.x = state[7];
+    odom.pose.pose.orientation.y = state[8];
+    odom.pose.pose.orientation.z = state[9];
+    odom.twist.twist.angular.x = state[10];
+    odom.twist.twist.angular.y = state[11];
+    odom.twist.twist.angular.z = state[12];
+    _state_pub.publish(odom);
+
+    tf::Transform transform;
+    transform.setOrigin( tf::Vector3(state[1], state[0], -state[2]) );
+    transform.setRotation( tf::Quaternion( state[8], state[7], -state[9], state[6] ));
+    tf_br->sendTransform(tf::StampedTransform(transform, ros::Time::now(), "world", "quad_body"));
+}
+
+void rcv_slam_odom_cb(nav_msgs::Odometry msg)
+{
+    quad.send_odom_data(msg.pose.pose.position.x, -msg.pose.pose.position.y, -msg.pose.pose.position.z,
+                        -msg.pose.pose.orientation.x, msg.pose.pose.orientation.w, -msg.pose.pose.orientation.z, msg.pose.pose.orientation.y);
+}
+
+void rcv_thrust_rates_cb(px4_bridge::ThrustRates msg)
+{
+    // quad.send_control_u(-msg.az/20, msg.wx, msg.wy, msg.wz);
+    quad.send_control_u(msg.thrust, msg.wx, msg.wy, msg.wz);
+}
+
+int main(int argc, char** argv)
+{
+    ros::init(argc, argv, "px4");
+    ros::NodeHandle n("~");
+
+    _imu_pub = n.advertise<sensor_msgs::Imu>("imu", 1);
+    _state_pub = n.advertise<nav_msgs::Odometry>("odom", 1);
+    _slam_odom_sub = n.subscribe("slam_odom", 1, rcv_slam_odom_cb, ros::TransportHints().tcpNoDelay());
+    _thrust_rates_sub = n.subscribe("thrust_rates", 1, rcv_thrust_rates_cb, ros::TransportHints().tcpNoDelay());
+    // _slam_odom_sub = n.subscribe("slam_odom", 1, rcv_slam_odom_cb);
+    // _thrust_rates_sub = n.subscribe("thrust_rates", 1, rcv_thrust_rates_cb);
+    tf_br = new tf::TransformBroadcaster();
+
+    quad.registe_rcv_state_callback(rcv_state_callback);
+    quad.registe_rcv_sensor_imu_callback(rcv_imu_callback);
+
+    quad.set_thread_rt(90);
+    if(quad.setup_port("/dev/ttyACM0") == -1)
+    {
+        exit(-1);
+    }
+
+    quad.setup_optitrack("192.168.1.200");
+    quad.add_fordwarding("192.168.1.22", 8976, "192.168.1.35", 14550);
+    // quad.add_fordwarding("127.0.0.1", 8976, "127.0.0.1", 14550);
+    quad.core_start();
+
+    ros::spin();
+    // ros::Rate loop_rate(100);
+    // while(ros::ok())
+    // {
+    //     ros::spinOnce();
+    //     loop_rate.sleep();
+    // }
+
+    return 0;
+}
